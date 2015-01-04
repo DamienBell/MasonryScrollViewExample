5a174ff174e77c8f495f5c2654b12a3286667445
Damien Bell <damiensbell@gmail.com>
2015-01-04 13:46:39 -0600

    Initial Commit

---
+++ MasonryScrollViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasonryScrollViewExample
+//
+//  Created by Damien Bell on 1/4/15.
+//  Copyright (c) 2015 Damien Bell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
